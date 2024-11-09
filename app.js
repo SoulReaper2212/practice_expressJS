be667e8c8d59f97b2609a4020b33ab7c6482d321
mmfdDevs <it.mmfd@gmail.com>
2024-11-09 12:52:15 +0500

    Added morgan in app js

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan'); // for logs
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(morgan('combined')) //for logs
 
 const userRoutes = require('./routes/user_routes');
 const PORT = 3000; 
